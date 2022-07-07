548139d4f91af3e97ddb3fd3dc3ad80603172938
dymafr <erwan1309@hotmail.com>
2022-07-07 15:19:57 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 function App() {
-  const { register, handleSubmit, getValues, watch } = useForm();
-
-  watch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: '',
+    },
+  });
 
   function submit(values) {
     console.log(values);
   }
-
-  console.log(getValues());
 
   return (
     <div
@@ -22,7 +26,40 @@ function App() {
           <label htmlFor="name" className="mb-5">
             Nom
           </label>
-          <input id="name" type="text" {...register('name')} />
+          <input
+            id="name"
+            type="text"
+            {...register('name', {
+              disabled: false,
+              required: 'Le champ est obligatoire',
+              maxLength: { value: 25, message: 'Trop long !' },
+              minLength: { value: 2, message: 'Trop court !' },
+              validate(value) {
+                if (value === 'Jean') {
+                  return true;
+                } else {
+                  return 'Mauvais prénom';
+                }
+              },
+            })}
+          />
+          {errors?.name && (
+            <p style={{ color: 'red' }}>{errors.name.message}</p>
+          )}
+        </div>
+        <div className="d-flex flex-column mb-20">
+          <label htmlFor="name" className="mb-5">
+            Age
+          </label>
+          <input
+            id="age"
+            type="number"
+            {...register('age', {
+              valueAsNumber: true,
+              required: 'Champ requis',
+            })}
+          />
+          {errors?.age && <p style={{ color: 'red' }}>{errors.age.message}</p>}
         </div>
         <button className="btn btn-primary">Sauvegarder</button>
       </form>
